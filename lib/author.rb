9e0e6bcb33c47f0a3d3c069142c3d137aeda1524
Snigdha Sur <Snigdha@Snigdhas-MacBook-Air-2.local>
2017-06-30 08:59:41 -0400

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,10 @@
+class Author 
+
+attr_accessor :name
+
+def initialize(name = nil)
+	@name = name
+end 
+
+
+end 
---
+++ lib/song.rb
@@ -0,0 +1,9 @@
+class Song
+
+attr_accessor :title, :artist 
+
+def initialize(title = nil)
+	@title = title
+end 
+
+end 
